432e7aeadb6c8e5ec8cae8db940f793bda79a10b
Luis <lgd26308@gmail.com>
2024-04-26 14:33:02 -0700

    removed unneeded code

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ let gfs
 conn.once('open', () => {
     // Ensure that the connection to the database is open before using GridFSBucket
     gfs = new GridFSBucket(conn.db, {
-        bucketName: 'images' // replace 'yourBucketName' with your actual bucket name
+        bucketName: 'images' 
     });
     console.log('GridFS Bucket has been initialized');
 });
@@ -23,10 +23,9 @@ router.route('/').get((req, res) => {
 
 router.route('/upload').post(upload.single('image'), (req, res) => {
     let filename = req.file.filename
-    // console.log(req.file)
     let newImage = new Image({ filename })
+    
     newImage.save().then((image) => {
-        // console.log(image)
         res.json(image)
     }).catch(err => {
         console.log(err)
@@ -35,21 +34,18 @@ router.route('/upload').post(upload.single('image'), (req, res) => {
 
 router.route('/image/:filename').get(async (req, res)=> {
     const filename = req.params.filename;
-    console.log('image requested:', filename)
-    console.log('gfs:', gfs)
-
     const cursor = await gfs.find({filename})
     const files = await cursor.toArray()
-    console.log('files:', files)
+
     if (!files || files.length === 0) {
         return res.status(404).json({
             err: 'no files exist'
         })
     }
+
     const file = files[0];
     const readStream = gfs.openDownloadStream(file._id);
     readStream.on('error', error => {
-        console.error('Error streaming file:', error);
         res.status(500).json({ error: 'Error streaming file' });
     });
     readStream.pipe(res);
